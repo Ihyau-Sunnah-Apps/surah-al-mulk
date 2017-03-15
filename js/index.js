96f674b5d8691c331211d7baadc7663562dbc394
Said Muhammad <saidmsaid81@gmail.com>
2017-03-16 00:12:39 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        $scope.addNotification = function (){
 	var today = new Date();
 		var tomorrow = new Date();
 		tomorrow.setDate(today.getDate());
