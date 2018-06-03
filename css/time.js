6ac5515ece7b583f7e112f55d78b3fd804421435
Said Muhammad <saidmsaid81@gmail.com>
2018-06-03 15:37:05 +0300

    Updated UI and improved code

---
+++ css/time.js
@@ -0,0 +1,47 @@
+function setNotification() {
+    var timeSet = $('#time-1').val();
+    var hour = timeSet.substring(0, 2);
+    var hourInt = parseInt(hour);
+    var minute = timeSet.substring(3,5);
+    var minuteInt = parseInt(minute);
+    document.getElementById('night-time-set').style.color = 'black';
+    document.getElementById('night-time-set').innerHTML = 'Success! Currently set to Every Night at ' + timeSet;
+
+    var night = new Date();
+    night.setDate(night.getDate());
+    var every =new Date();
+    every.setDate(night.getDate());
+    every.setHours(hourInt);
+    every.setMinutes(minuteInt);
+    every.setSeconds(0);
+    var every_night = new Date(every);
+
+
+  cordova.plugins.notification.local.schedule({
+    id: 1,
+    title: 'Reminder to recite Suratul Kahf',
+    text: 'Tap here to read now',
+    trigger:{
+    firstAt: every_night,
+    every: "day"
+        },
+    icon: 'res://icon',
+    smallIcon: 'res://ic_popup_reminder',
+    });window.plugins.toast.showLongTop("Successfully Enabled");
+  
+}
+
+
+  
+  cordova.plugins.notification.local.on("click", function (notification, state) {
+        AdMob.hideBanner(); window.open('pages.html');
+        }, this);
+  
+
+        
+
+  
+        
+       
+    
+
---
+++ js/video.js
@@ -0,0 +1,44 @@
+function doesitexistm() {
+
+var permissions = cordova.plugins.permissions;
+permissions.checkPermission(permissions.WRITE_EXTERNAL_STORAGE, openFile(), permissions.requestPermission(permissions.WRITE_EXTERNAL_STORAGE));
+
+}
+
+function openFilem() {
+	$.ajax({
+url:'file:///storage/emulated/0/Download/Surah Al-Mulk.mp4',
+type:'HEAD',
+error: function()
+{
+    //file not exists
+function onConfirm(buttonIndex) {
+  if (buttonIndex==1) {
+window.open('https://www.dropbox.com/s/ttsk9m2ttb8idsl/Surah%20Al-Kahf.mp4?dl=1','_system')
+} else if (buttonIndex==2) {
+ $("#popupvideom").popup("open");
+} else {
+
+};
+    
+};
+navigator.notification.confirm(
+    'Would you like to download the file so as its available offline next time or Just play without downloading?', // message
+     onConfirm,            // callback to invoke with index of button pressed
+    'Download/Play (Abdurrahman As-Sudais',           // title
+    ['Download (49.5MB)','Play without downloading']     // buttonLabels
+);
+},
+success: function()
+{
+    //file exists
+window.plugins.toast.showLongBottom('Starting player please wait...', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});playVideo(document.getElementById("vidUrl").value);
+}
+});
+
+}
+
+       
+
+
+      
