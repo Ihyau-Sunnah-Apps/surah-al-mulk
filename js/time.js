7f1da4ccd32827d8536a99cedbc6d00a4f4afffb
Said Muhammad <saidmsaid81@gmail.com>
2018-06-18 17:52:22 +0300

    Testing action buttons on notifications

--- js/time.js
+++ js/time.js
@@ -20,9 +20,14 @@ function setNotification() {
                     minute: minuteInt
                 }
         },
+    actions: [
+        { id: 'remind', title: 'Remind Others' }
+    ]
     icon: 'res://icon',
     smallIcon: 'res://ic_popup_reminder',
     });window.plugins.toast.showLongTop("Successfully Enabled");
+
+  cordova.plugins.notification.local.on('remind', function (notification, eopts) { window.plugins.socialsharing.share('I am reminding you to read Surah Al Mulk and Al Sajdah its the Sunnah of our beloved Prophet Muhammad (peace and blessings of Allaah be upon him).\n\nIt was narrated that ‘Abd-Allaah ibn Mas’ood said: \n\nWhoever readsTabaarak allaahi bi yadihi’l-mulk[i.e., Soorat al-Mulk] every night, Allaah will protect him from the torment of the grave. At the time of the Messenger of Allaah (peaceand blessings of Allaah be upon him) we used to call ital- maani’ah(that which protects). In the Book of Allaah it is a soorah which, whoever recites it every night has done very well. \n\nNarrated by al-Nasaa’i, 6/179;\n\nIt was narrated from Jaabir, that the Prophet (peace and blessings of Allaah be upon him) never used to sleep until he had recitedAlif-laam-meem tanzeel[al-Sajdah] andTabaarak alladhi bi yadihi’l-mulk[al-Mulk].\n\n Narrated by al-Tirmidhi, 2892; Ahmad, 14249. Al-Albaani said inSaheeh al-Tirmidhi(3/6) that this hadeeth is saheeh.\n\nSent from Surah Al Mulk android App http://bit.ly/mulk-app\n\nDont have an android phone read online', null, null, 'thesunnahrevival.github.io'); });
   
 }
 
