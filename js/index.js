19673128b44ec68d11897e303f679d8aa7b7756f
saidmsaid81 <saidmsaid81@gmail.com>
2020-09-23 17:52:18 +0300

    Ending support for the app

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 document.addEventListener('deviceready', this.onDeviceReady, false);
      function onDeviceReady() {
-        welcomeScreen();
+         var date = new Date();
+         var day = date.getDate();
+         var update = window.localStorage.getItem("update");
+         if(update != day){
+            window.localStorage.setItem("update", day);
+         }
+         else {
+            document.getElementById("update").style.display = "none"   
+         }
         AppRate.preferences.storeAppURL.android = 'market://details?id=com.ihyausunnah.surahalkahfmulk';
         AppRate.preferences.openStoreInApp = true;
         AppRate.preferences.displayAppName = 'Surah Al Mulk App';
